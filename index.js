0ffad3f6a98e7b0e5048e7db696bda49270181c5
Ryan Munz <ryan.munzy@gmail.com>
2022-12-13 16:56:29 -0500

    commit tree 6c9b4a2a87b68aadb638b9dd97bb4e0a19a897d8

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+const activity = 'Soccer';
+
+function saturdayFun (activity) {
+    if (activity === NaN) return "This Saturday, I want to roller-skate";
+    else return `This Saturday, I want to ${activity}!`;
+}
